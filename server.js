c628970b3e24ac5d08b6b5e3db388e77326e2b0f
Sreeja Guduguntla <sreejaguduguntla@gmail.com>
2026-05-18 11:38:37 +0530

    Backend deployment setup

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ config(); //process.env
 //Create express application
 const app = exp();
 //use cors middleware
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
 //add body parser middleware
 app.use(exp.json());
 //add cookie parser middleware
